554e9e2ea0cdb890b777c4d8eb8bfee5175c8810
Satyajit24behera <106335803+Satyajit24behera@users.noreply.github.com>
2025-11-27 12:05:54 +0530

    Update condition_matcher.py

--- utils/condition_matcher.py
+++ utils/condition_matcher.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import streamlit as st
 load_dotenv()
-genai.configure(api_key="AIzaSyCMkDMaIvv2O33FVB25159qEKXyO0Uz9sY")
+genai.configure(api_key=os.getenv(API_KEY))
 
 def match_conditions(user_data: dict, medical_report: str = "") -> str:
     prompt = f"""
@@ -23,6 +23,7 @@ def match_conditions(user_data: dict, medical_report: str = "") -> str:
     model = genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content(prompt)
     return response.text
+
 
 
 
